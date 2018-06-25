97db5b75fc6774137857f3eef8cf2a758745010a
Modest4 <stephe777@gmail.com>
2018-06-25 16:23:48 +0200

    Edit the mentions, turn the delete phase process into a function, add a TODO and edit the instructions of the README.md

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-"# russian-delete" 
+"# russian-delete" by Modest4
 
 Would you have the balls to run this evil nodeJS program on your computer ?
 Challenge your coworkers during happy hours and pray for your files !
+
+You can either specify a specific folder or let the Gods decide of your destiny.
+
+Here are the differents levels available :
+	- pussy (potentially delete a random file in a specific folder) + Required path to folder
+	- russian-pussy (delete a random file or folder in a specific fodler) + Required path to folder
+	- cow-boy (potentially delete a random file somewhere)
+	- russian (delete a file)
+	- drunk-russian (delete a folder HELL YEA)
--- delete.js
+++ delete.js
@@ -1,19 +1,80 @@
+/*
+	  THIS CODE WAS WRITTEN BY MODESTA THE GREAT
+	  https://github.com/Modest4/russian-delete
+
+		                     _           _        
+		                    | |         | |       
+		 _ __ ___   ___   __| | ___  ___| |_ __ _ 
+		| '_ ` _ \ / _ \ / _` |/ _ \/ __| __/ _` |
+		| | | | | | (_) | (_| |  __/\__ \ || (_| |
+		|_| |_| |_|\___/ \__,_|\___||___/\__\__,_|
+
+				    _               _
+				   /.)    PRAY     (.\
+				  /)\|   MODEST4   |/(\
+				 //)/               \(\\
+				/'"^"               "^"`\
+*/
+
+
 const fs = require('fs')
+
+let levels = ["pussy", "russian-pussy", "cow-boy", "russian", "drunk-russian"]
+let lvl = process.argv[2]
+/*
+	Here are the differents levels available :
+		- pussy (potentially delete a random file in a specific folder) + Required path to folder
+		- russian-pussy (delete a random file or folder in a specific fodler) + Required path to folder
+		- cow-boy (potentially delete a random file somewhere)
+		- russian (delete a file)
+		- drunk-russian (delete a folder HELL YEA)
+*/
+let folderPath = process.argv[3]
 let dir = "./tmp"
+
+if (lvl === undefined) throw "What kind of game do you wanna play? Are you a pussy, a russian-pussy, a cow-boy, a russian or a drunk-russian ? How stupid are you dude?"
+if (folderPath === undefined && lvl === levels[0] || folderPath === undefined && lvl === levels[1]) throw `You, ${lvl}, have to tell me in which folder I have to work`
+
+let deleteFile = function(filePath) {
+	fs.unlink(filePath, (err) => {
+		if (err){
+			return err
+		}
+	})
+}
 
 fs.readdir(dir, (err, files) => {
 	if (err) throw err
-	files.forEach((file) => {
-		if(fs.lstatSync(dir + "/" + file).isFile()) {
-			fs.unlink(dir + "/" + file, (err) => {
-				if (err){
-					console.log(err)
-				} else {
-					console.log(`${file} a été supprimé !`);
-				}
-			})
-		} else {
-			console.log("c'est un dossier, non un fichier", file)
+
+	let theFile = Math.floor(Math.random() * files.length)
+	console.log(`On va supprimer le fichier ${files[theFile]}`)
+
+	files.forEach((file, position) => {		
+		if(position === theFile) {		/* check if each of the content is a file */
+			if (fs.lstatSync(dir + "/" + file).isFile()) {
+				let fullPath = dir + "/" + file
+				deleteFile(fullPath, (err) => {
+					if (err) {
+						throw err
+					} else {
+						console.log(`${file} a été supprimé`)
+					}
+				})
+			} else {
+				console.log("c'est un dossier, non un fichier", file)
+			}
 		}
 	})
 })
+
+
+
+/*
+	TODO
+
+	✓ Function for deleting files ✓
+	✘ Function for deleting folder ✘
+	✘ Move this into a module ✘
+	❄ Create a module which recursively search a random place on the disk ❄
+
+*/
